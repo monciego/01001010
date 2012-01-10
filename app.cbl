d1bd3f9d7c1b0777665a274ba5b6ce720bb37de1
Jericho <jerichobantiquete@gmail.com>
2012-01-10 13:18:12 +0800

    0b1

---
+++ app.cbl
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       
+       ENVIRONMENT DIVISION.
+       
+       DATA DIVISION.
+       
+       PROCEDURE DIVISION.
+       000-MAIN-PROCEDURE.
+           DISPLAY "Hello World!".
+           STOP RUN.
